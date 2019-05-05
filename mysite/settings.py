9adedc750c4d071ca01737e10aa59c67f9ea2ac0
Vitaly Polishchuk <fox104@mail.ru>
2019-05-05 15:30:26 +0300

    Deleted Debug

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'x901)gwy^$!rfy&9gba7hj4p0n-=j8!&63su6g(uf=fa52uj=@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
